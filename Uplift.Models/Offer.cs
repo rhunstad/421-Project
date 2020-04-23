7aefa9daad7489d51834b479bb577ef71df4f286
Kat Glueckert <kaglueckert@crimson.ua.edu>
2020-04-23 15:27:59 -0500

    addedTables

---
+++ Uplift.Models/Offer.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Text;
+
+namespace Uplift.Models
+{
+     public class Offer
+    {
+
+        
+        [Required]
+        public Guid ItemID { get; set; }
+
+       [Required]
+       public string Fname { get; set; }
+
+        [Required]
+        public string LName { get; set; }
+
+        [Required]
+        public string buyerName { get; set; }
+
+        [Required]
+        public DateTime offerDate { get; set; }
+
+       
+
+        
+  
+    }
+}
