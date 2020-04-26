025aeacfa3a4009239dc666b851a0b5ddfd1b97f
rhunstad <rhunstad@crimson.ua.edu>
2020-04-26 00:28:31 -0600

    Filled home page categories

--- Uplift/Areas/Customer/Controllers/HomeController.cs
+++ Uplift/Areas/Customer/Controllers/HomeController.cs
@@ -52,34 +52,86 @@ namespace Uplift.Controllers
             //displayItem.ItemDescription = "This is the item description2. This is the item descriptionThis is the item description3. This is the item descriptionThis is the item description4. This is the item descriptionThis is the item description5. This is the item descriptionThis is the item description6. This is the item descriptionThis is the item description7. This is the item descriptionThis is the item description8. This is the item descriptionThis is the item description9. This is the item descriptionThis is the item description10. This is the item description";
             //FeaturedItems.Add(displayItem);
 
-            //List<Item> SuppliesItems = new List<Item>();
-            //List<Item> DormFurnitureItems = new List<Item>();
-            //List<Item> ElectronicsItems = new List<Item>();
-            //List<Item> WomensClothesItems = new List<Item>();
-            //List<Item> MensClothesItems = new List<Item>();
-            //List<Item> AccessoriesItems = new List<Item>();
-            //List<Item> ServicesItems = new List<Item>();
-            //List<Item> OtherItems = new List<Item>();
-            //List<Item> BrowseAllItems = new List<Item>();
+            List<Item> FeaturedItems = new List<Item>();
+            List<Item> SuppliesItems = new List<Item>();
+            List<Item> DormFurnitureItems = new List<Item>();
+            List<Item> ElectronicsItems = new List<Item>();
+            List<Item> WomensClothesItems = new List<Item>();
+            List<Item> MensClothesItems = new List<Item>();
+            List<Item> AccessoriesItems = new List<Item>();
+            List<Item> ServicesItems = new List<Item>();
+            List<Item> OtherItems = new List<Item>();
+            List<Item> BrowseAllItems = new List<Item>();
 
-            //ItemsList.FeaturedItems = FeaturedItems;
-            //ItemsList.SuppliesItems = SuppliesItems;
-            //ItemsList.DormFurnitureItems = DormFurnitureItems;
-            //ItemsList.ElectronicsItems = ElectronicsItems;
-            //ItemsList.WomensClothesItems = WomensClothesItems;
-            //ItemsList.MensClothesItems = MensClothesItems;
-            //ItemsList.AccessoriesItems = AccessoriesItems;
-            //ItemsList.ServicesItems = ServicesItems;
-            //ItemsList.OtherItems = OtherItems;
-            //ItemsList.BrowseAllItems = BrowseAllItems;
+
 
             var ItemsList = _unitOfWork.Item.GetAll();
-            if (ItemsList == null)
+            dynamic ViewModel = new ExpandoObject();
+            var count = 0;
+            var featured1 = 3;
+            var featured2 = 5;
+            var featured3 = 7;
+            var featured4 = 1;
+
+            foreach (var Item in ItemsList)
+            {
+                if ((Item.ItemCategory == "Supplies") & (SuppliesItems.Count < 4))
+                {
+                    SuppliesItems.Add(Item);
+                }
+                else if ((Item.ItemCategory == "Dorm Furniture") & (DormFurnitureItems.Count < 4))
+                {
+                    DormFurnitureItems.Add(Item);
+                }
+                else if ((Item.ItemCategory == "Electronics") & (ElectronicsItems.Count < 4))
+                {
+                    ElectronicsItems.Add(Item);
+                }
+                else if ((Item.ItemCategory == "Clothes-Men") &(MensClothesItems.Count < 4))
+                {
+                    MensClothesItems.Add(Item);
+                }
+                else if ((Item.ItemCategory == "Clothes-Women") & (WomensClothesItems.Count < 4))
+                {
+                    WomensClothesItems.Add(Item);
+                }
+                else if ((Item.ItemCategory == "Clothes-Accessories") & (AccessoriesItems.Count < 4))
+                {
+                    AccessoriesItems.Add(Item);
+                }
+                else if ((Item.ItemCategory == "Services") & (ServicesItems.Count < 4))
+                {
+                    ServicesItems.Add(Item);
+                }
+                if ((count == featured1) | (count == featured2) | (count == featured3) | (count == featured4))
+                {
+                    FeaturedItems.Add(Item);
+                }
+                if ((count == 0) | (count == 1) | (count == 2) | (count == 3))
+                {
+                    BrowseAllItems.Add(Item);
+                }
+                count += 1;
+            }
+
+
+            ViewModel.FeaturedItems = FeaturedItems;
+            ViewModel.SuppliesItems = SuppliesItems;
+            ViewModel.DormFurnitureItems = DormFurnitureItems;
+            ViewModel.ElectronicsItems = ElectronicsItems;
+            ViewModel.WomensClothesItems = WomensClothesItems;
+            ViewModel.MensClothesItems = MensClothesItems;
+            ViewModel.AccessoriesItems = AccessoriesItems;
+            ViewModel.ServicesItems = ServicesItems;
+            ViewModel.OtherItems = OtherItems;
+            ViewModel.BrowseAllItems = BrowseAllItems;
+
+            if (ViewModel == null)
             {
                 return NotFound();
             }
 
-            return View(ItemsList);
+            return View(ViewModel);
 
         }
 
